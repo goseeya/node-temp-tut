cd569d4c512f0c5cab72ce77092169df44cb16fb
goseeya <149542@gmail.com>
2022-11-01 20:19:27 +0100

    add-listening to server

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-const EventEmitter = require('events')
+// const { writeFileSync } = require('fs')
+// for (let i=0; i<10000; i++) {
+//     writeFileSync('./content/big.txt', `hello world ${i}\n`, {flag: 'a'})
+// }
+
+// const {createReadStream} = require('fs')
+
+// const stream = createReadStream('./content/big.txt', {highWaterMark: 9000, encoding: 'utf8'});
+
+// stream.on('data', (result)=> {
+//     console.log(result);
+// })
+
+// stream.on('error', (err)=>console.log(err);)
+
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(function(req,res){
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+    fileStream.on('open', () => {
+        fileSgtream.pipe(res)
+    })
+    fileStream.on('error',(err)=>{
+        res.end(err)
+    })
+})
+.listen(5000)
